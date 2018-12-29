d4ac024b47c693da95dfb6fa8d5c2dc301971250
Ryan Gill <rdgill95@gmail.com>
2018-12-29 16:02:00 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,8 +1,8 @@
 def roll_call_dwarves(dwarves)
   dwarves.each_with_index do |item, index|
     unshift(index)
-    
-    end
+    puts item
+  end
 end
 
 def summon_captain_planet# code an argument here
