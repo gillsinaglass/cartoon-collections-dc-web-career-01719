aa9e79b68f0a97d6b2d1ffb5d5d02076ccae9978
Ryan Gill <rdgill95@gmail.com>
2018-12-29 16:24:36 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -21,7 +21,7 @@ def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
   if array.include?(cheese_types) == true
     array.find do |x|
-      
-    
+      x == cheese_types
+    end
   end
 end
