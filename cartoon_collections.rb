2b178b9d93e598cbcbca199337bd423da2b391d0
Ryan Gill <rdgill95@gmail.com>
2018-12-29 16:11:06 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -11,8 +11,10 @@ def summon_captain_planet(element)
   end
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(calls)
+  calls.include? do |word|
+    word.length > 4
+  end
 end
 
 def find_the_cheese# code an argument here
